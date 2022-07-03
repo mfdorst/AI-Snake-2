2693db09b247634508ea552a082cec405a71da0c
Michael Dorst <m@mdorst.net>
2022-07-03 00:42:57 -0700

    Add example canvas rendering code

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod snake;
 use js_sys::Function;
 use snake::*;
 use std::cell::RefCell;
+use std::f64;
 use std::rc::Rc;
 use wasm_bindgen::{prelude::*, JsCast, UnwrapThrowExt};
 use web_sys::window;
@@ -17,6 +18,7 @@ thread_local! {
 
 #[wasm_bindgen(start)]
 pub fn main() {
+    render();
     TICK_CLOSURE.with(|tick_closure| {
         window()
             .unwrap_throw()
@@ -26,4 +28,43 @@ pub fn main() {
             )
             .unwrap_throw();
     });
+}
+
+pub fn render() {
+    let document = web_sys::window().unwrap_throw().document().unwrap_throw();
+    let canvas = document.get_element_by_id("canvas").unwrap_throw();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap_throw();
+    let context = canvas
+        .get_context("2d")
+        .unwrap_throw()
+        .unwrap_throw()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap_throw();
+    context.begin_path();
+    // Draw the outer circle
+    context
+        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap_throw();
+
+    // Draw the mouth
+    context.move_to(110.0, 75.0);
+    context
+        .arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI)
+        .unwrap_throw();
+
+    // Draw the left eye.
+    context.move_to(65.0, 65.0);
+    context
+        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the right eye.
+    context.move_to(95.0, 65.0);
+    context
+        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    context.stroke();
 }
